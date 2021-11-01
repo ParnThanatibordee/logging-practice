a80c4d7eaee38d6c9a074a7976a9a2d68445b20b
ParnThanatibordee <parn8946@gmail.com>
2021-11-02 01:31:51 +0700

    edit my_config

--- logging_demo.py
+++ logging_demo.py
@@ -47,9 +47,8 @@ def simple_config():
 
 def my_config():
     """Write your own logging configuration."""
-    # TODO write your own logging configuration
-    #      specify a log file, threshold level, format, and append mode
-    pass
+    FORMAT = '%(filename)s %(levelname)s: %(message)s'
+    logging.basicConfig(format=FORMAT, level=logging.DEBUG, filename='general.log')
 
 
 if __name__ == "__main__":
@@ -59,12 +58,12 @@ if __name__ == "__main__":
 
     # 2. Call simple_config to set the format of log messages.
     #    Comment out the above call (#1) to basicConfig for this.
-    simple_config()
+    # simple_config()
 
     # 3. my_config() write your own logging configuration as
     #    described in the assignment. 
     #    Comment out the above calls to simple_config and basicConfig.
-    # my_config() 
+    my_config()
 
     # Log some messages to the root logger using different logging levels.
     logger = logging.getLogger()
